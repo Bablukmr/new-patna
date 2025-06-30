dca799020a5f5b70168e715e932a1497ff8e568e
= <=>
2025-06-30 12:42:37 +0530

    token added

--- services/getApi.ts
+++ services/getApi.ts
@@ -2,8 +2,8 @@ import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/api-response";
 
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const TOKEN = process.env.NEXT_PUBLIC_API_TOKEN!;
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://patnazooapi.fillipsoftware.com/api";
+const TOKEN = process.env.NEXT_PUBLIC_API_TOKEN || "3|8eTLf8lYwd5cFqSRv3AykMESHtTYEZQwSFpcO3w9ba6d3f86";
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
